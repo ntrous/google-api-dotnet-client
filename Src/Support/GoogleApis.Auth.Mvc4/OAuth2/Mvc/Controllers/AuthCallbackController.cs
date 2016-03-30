62069375e56fe990b11cd1331686eef75bcad7a2
Navarre Trousselot <ntrous@gmail.com>
2016-03-30 21:08:38 +1100

    Created WebApi project

--- Src/Support/GoogleApis.Auth.Mvc4/OAuth2/Mvc/Controllers/AuthCallbackController.cs
+++ Src/Support/GoogleApis.Auth.Mvc4/OAuth2/Mvc/Controllers/AuthCallbackController.cs
@@ -16,12 +16,12 @@ limitations under the License.
 
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 using Google.Apis.Auth.OAuth2.Flows;
 using Google.Apis.Auth.OAuth2.Mvc.Filters;
 using Google.Apis.Auth.OAuth2.Responses;
 using Google.Apis.Auth.OAuth2.Web;
 using Google.Apis.Logging;
+using System.Web.Mvc;
 
 namespace Google.Apis.Auth.OAuth2.Mvc.Controllers
 {
---
+++ Src/Support/GoogleApis.Auth.WebApi/OAuth2/WebApi/Filters/AuthActionFilter.cs
@@ -0,0 +1,55 @@
+﻿/*
+Copyright 2013 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using Google.Apis.Auth.OAuth2.Responses;
+using Microsoft.AspNet.Mvc.Filters;
+using System.Web;
+
+namespace Google.Apis.Auth.OAuth2.WebApi.Filters
+{
+    /// <summary>
+    /// An action filter which parses the query parameters into
+    /// <see cref="Google.Apis.Auth.OAuth2.Responses.AuthorizationCodeResponseUrl"/>.
+    /// </summary>
+    public class AuthorizationCodeActionFilter : ActionFilterAttribute, IActionFilter
+    {
+        /// <summary>
+        /// Parses the request into <see cref="Google.Apis.Auth.OAuth2.Responses.AuthorizationCodeResponseUrl"/>.
+        /// </summary>
+        public override void OnActionExecuting(ActionExecutingContext actionContext)
+        {
+            (actionContext.ActionArguments["authorizationCode"] as AuthorizationCodeResponseUrl)
+                .ParseRequest(actionContext.HttpContext.Request);
+
+            base.OnActionExecuting(actionContext);
+        }
+    }
+
+    /// <summary>Auth extensions methods.</summary>
+    public static class AuthExtensions
+    {
+        /// <summary>Parses the HTTP request query parameters into the Authorization code response.</summary>
+        internal static void ParseRequest(this AuthorizationCodeResponseUrl authorizationCode, Microsoft.AspNet.Http.HttpRequest request)
+        {
+            var queryDic = HttpUtility.ParseQueryString(request.Query.ToString());
+            authorizationCode.Code = queryDic["code"];
+            authorizationCode.Error = queryDic["error"];
+            authorizationCode.ErrorDescription = queryDic["error_description"];
+            authorizationCode.ErrorUri = queryDic["error_uri"];
+            authorizationCode.State = queryDic["state"];
+        }
+    }
+}
